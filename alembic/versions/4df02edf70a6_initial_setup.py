621b65de0a851d5b91cf454f1ee7f16b220d0b96
nicolemcardenas <nicmocard@gmail.com>
2026-05-12 13:29:51 -0600

    chore: configurar alembic y conexión PostgreSQL

---
+++ alembic/versions/4df02edf70a6_initial_setup.py
@@ -0,0 +1,28 @@
+"""initial setup
+
+Revision ID: 4df02edf70a6
+Revises: 
+Create Date: 2026-05-08 15:53:47.057517
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '4df02edf70a6'
+down_revision: Union[str, Sequence[str], None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
